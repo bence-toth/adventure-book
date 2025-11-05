a7fb55b92ee0eb5ee25de9f01687d0ad5dda413a
Bence A. Toth <tothab@gmail.com>
2025-11-05 22:36:46 +0100

    Fixed linter errors

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,18 +1,47 @@
+/* eslint-disable react-refresh/only-export-components */
 import { render as rtlRender } from "@testing-library/react";
 import type { RenderOptions } from "@testing-library/react";
 import type { ReactElement } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+// Wrapper component for providers
+function AllTheProviders({ children }: { children: React.ReactNode }) {
   return <BrowserRouter>{children}</BrowserRouter>;
-};
+}
 
+// Custom render function
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
 ) => rtlRender(ui, { wrapper: AllTheProviders, ...options });
 
-// Re-export everything
-export * from "@testing-library/react";
-// Override render method
+// Re-export specific testing utilities to avoid export * issues
+export {
+  screen,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+  getByRole,
+  getByText,
+  getByLabelText,
+  getByPlaceholderText,
+  getByAltText,
+  getByDisplayValue,
+  queryByRole,
+  queryByText,
+  queryByLabelText,
+  queryByPlaceholderText,
+  queryByAltText,
+  queryByDisplayValue,
+  findByRole,
+  findByText,
+  findByLabelText,
+  findByPlaceholderText,
+  findByAltText,
+  findByDisplayValue,
+  act,
+} from "@testing-library/react";
+
+// Export custom render as default render
 export { customRender as render };
