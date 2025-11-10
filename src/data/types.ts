dcde167a993fea28fe6d14183e61fe9e0b9e3ef0
Bence A. Toth <tothab@gmail.com>
2025-11-10 13:40:21 +0100

    Fix type and validation mismatch

--- src/data/types.ts
+++ src/data/types.ts
@@ -24,7 +24,7 @@ export type RawPassage =
       ending: true;
       type?: "victory" | "defeat" | "neutral";
       choices?: never;
-      effects?: Effect[];
+      effects?: never;
     }
   | {
       text: string;
